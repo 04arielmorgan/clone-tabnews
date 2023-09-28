029e45feaffc28334e8ffb23654de41505e02b15
Ariel Morgan <morgnariel@gmail.com>
2023-09-28 19:39:32 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-
 function Home() {
-    return(
-        <div>
-            <h2>Angélica, Eu te amo muito ❤️! Muito Mesmo!</h2>
-        </div>
-    )
+  return (
+    <div>
+      <h2>Angélica, Eu te amo muito ❤️! Muito Mesmo!</h2>
+    </div>
+  );
 }
 
 export default Home;
